3b5981211460a6a1290511574feb138846eb0145
Ajitesh Madai <ajitesh.j.madai@accenture.com>
2016-09-17 21:21:14 +0530

    Design Changes

--- js/onepush/controller/searchController.js
+++ js/onepush/controller/searchController.js
@@ -7,9 +7,6 @@ OnePushApp.controller("SearchController", [ '$http', function($http) {
 
 	var self = this;
 	
-	//self.websiteList = [];
-	
-	
 	self.fetchWebsiteList = function() {
 		console.debug("Fetching website list");
 		$http.get(OnePushApp.API_URL, {
@@ -18,17 +15,33 @@ OnePushApp.controller("SearchController", [ '$http', function($http) {
 				'query' : 'list_websites'
 			}
 		}).success(function(data) {
-			console.debug("Website list loaded successfully");
-			if(data && data.websites) {
-				self.websiteList = data.websites;
-			}
+			
+			self.processWebsiteList(data);
+			
 		}).error(function(a, b, c) {
 			console.error("Error occured when fetching website list",a,b,c);
 		});
 	};
 	
+	self.processWebsiteList = function(data) {
+		
+		console.debug("Website list loaded successfully");
+		if(data && data.websites) {
+			self.totalNoOfWebsites = data.websites.length;
+			if(self.totalNoOfWebsites > 10) {
+				self.websiteList = data.websites.slice(0,10);
+			} else {
+				self.websiteList = data.websites;
+			}
+			
+		} else {
+			console.error("Data recieved in incorrect format");
+		}
+		
+	};
+	
+	
 	self.fetchWebsiteList();
-	self.websiteListStr = JSON.stringify(self.websiteList);
 	
 	
 
